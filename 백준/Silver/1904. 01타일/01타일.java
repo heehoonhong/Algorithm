50f27ad6d8e13c772d95e507a43a48d56a53462c
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-03-04 21:46:21 +0900

    [Silver III] Title: 01타일, Time: 188 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -2,29 +2,44 @@
 
 import java.util.*;
 
+
+
 public class Main {
-    static int[] fib;
+	
+	public static int fibo(int n) {
+		
+		if(n==1) {
+			return 1;
+		}
+		
+		if(n==2) {
+			return 2;
+		}
+		
+		int val1=1;
+		int val2=2;
+		int sum=0;
+		
+		for(int i = 2;i<n;i++) {
+			sum=(val1+val2)%15746;
+			val1=val2;
+			val2=sum;
+		}
+		
+		return sum;
+		
+	}
 
-    public static int fibo(int n) {
-        if (fib[n] != -1) { // 이미 계산된 값이면 반환
-            return fib[n];
-        }
-        if (n == 1 || n == 2) { // 기본값 설정 (F(1) = 1, F(2) = 2)
-            return fib[n] = n;
-        }
-
-        // 재귀 호출 + 모듈러 연산 적용
-        return fib[n] = (fibo(n - 1) + fibo(n - 2)) % 15746;
-    }
-
+	
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
-
-        fib = new int[n + 1];
-        Arrays.fill(fib, -1); // 초기값 -1로 설정하여 메모이제이션 여부 확인 가능
-
+        
+        int n=sc.nextInt();
+        //fib=new int[n+1];
         System.out.println(fibo(n));
+        
     }
+
+	
 }
