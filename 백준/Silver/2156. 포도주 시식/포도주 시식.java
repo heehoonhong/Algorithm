a675854c3ef1a77298acfd97eef741d004ec94d0
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-03-28 15:10:38 +0900

    [Silver I] Title: 포도주 시식, Time: 156 ms, Memory: 17480 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,29 +1,38 @@
-import java.util.Scanner;
- 
+//package ctp;
+
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
- 
-		int[] arr = new int[N + 1];
-		int[] dp = new int[N + 1];
- 
-		for (int i = 1; i <= N; i++) {
-			arr[i] = in.nextInt();
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n=Integer.parseInt(br.readLine());
+		
+		int[] arr=new int[n+1];
+		int[] dp=new int[n+1];
+		
+		for(int i = 1;i<=n;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
 		}
- 
-		dp[1] = arr[1];
-		if (N > 1) {
-			dp[2] = arr[1] + arr[2];
-		}
-		for (int i = 3; i <= N; i++) {
-			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
- 
-		}
-		System.out.println(dp[N]);
+		
+		if (n >= 1) dp[1] = arr[1];
+        if (n >= 2) dp[2] = arr[1] + arr[2];
+        if (n >= 3) dp[3] = Math.max(dp[2], Math.max(arr[1] + arr[3], arr[2] + arr[3]));
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1],
+                        Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+        }
+		
+		bw.write(dp[n]+"\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
- 
 }
