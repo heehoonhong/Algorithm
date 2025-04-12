a467b7c0529e6157ff2c9f600fbd9161dc60b4fb
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-04-12 16:58:39 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 18784 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,68 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	
+	static int[] arr;
+	static int[] visited;
+	static BufferedWriter bw;
+	
+	public static void dfs(int n,int m,int depth) throws IOException {
+		
+		if(depth==m) {
+			for(int element:arr) {
+				bw.write(element+" ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i=1;i<=n;i++) {
+			if(depth==0) {
+				if(visited[i]==0) {
+					visited[i]=1;
+					arr[depth]=i;
+					dfs(n,m,depth+1);
+					visited[i]=0;
+				}
+			}
+			else {
+				if(visited[i]==0&&arr[depth-1]<=i) {
+					visited[i]=1;
+					arr[depth]=i;
+					dfs(n,m,depth+1);
+					visited[i]=0;
+				}
+				else if(visited[i]==1&&arr[depth-1]==i) {
+					visited[i]=1;
+					arr[depth]=i;
+					dfs(n,m,depth+1);
+					visited[i]=0;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String line=br.readLine();
+		StringTokenizer st=new StringTokenizer(line);
+		
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		
+		arr=new int[m];
+		visited=new int[n+1];
+		dfs(n,m,0);
+		
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
