974c5bd2cfa85ee768f635e0740af74df760ed4d
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-07-09 19:38:09 +0900

    [Gold V] Title: 치킨 배달, Time: 232 ms, Memory: 21320 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -8,46 +8,50 @@ import java.util.*;
 public class Main {
 	
 	static class Point{
-		int x;
-		int y;
-		Point(int x,int y) {
+		int x,y;
+		Point(int x,int y){
 			this.x=x;
 			this.y=y;
 		}
 	}
 	
-	public static int n,m,answer;
+	public static int maxChickenNum,mapRow;
+	public static List<Point> homeList=new ArrayList<>();
+	public static List<Point> chickenList=new ArrayList<>();
+	public static List<Point> selectedChickenList=new ArrayList<>();
 	public static int[][] map;
-	public static List<Point> home=new ArrayList<>();
-	public static List<Point> chicken=new ArrayList<>();
+	public static int answer;
 	
 	public static void main(String[] args) throws Exception {
-		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String line=br.readLine();
 		StringTokenizer st=new StringTokenizer(line);
-		n=Integer.parseInt(st.nextToken());
-		m=Integer.parseInt(st.nextToken());
-		map=new int[n][n];
 		
-		for(int i = 0;i<n;i++) {
+		mapRow=Integer.parseInt(st.nextToken());
+		maxChickenNum=Integer.parseInt(st.nextToken());
+		
+		map=new int[mapRow][mapRow];
+		
+		for(int i = 0;i<mapRow;i++) {
 			line=br.readLine();
 			st=new StringTokenizer(line);
-			for(int j = 0;j<n;j++) {
+			for(int j = 0;j<mapRow;j++) {
 				map[i][j]=Integer.parseInt(st.nextToken());
-				if(map[i][j]==1) {
-					home.add(new Point(i,j));
+				if(map[i][j]==1) { // 집인 경우
+					homeList.add(new Point(i,j));
 				}
-				else if(map[i][j]==2) {
-					chicken.add(new Point(i,j));
+				else if(map[i][j]==2) { // 치킨집인 경우
+					chickenList.add(new Point(i,j));
 				}
 			}
 		}
 		
 		answer=Integer.MAX_VALUE;
-		dfs(0,new ArrayList<>());
+		
+		dfs(0);
+		
 		bw.write(answer+"\n");
 		
 		br.close();
@@ -55,36 +59,42 @@ public class Main {
 		bw.close();
 	}
 	
-	public static void dfs(int currentChickenNum,List<Point> selectedChicken) {
-		if(selectedChicken.size()+(chicken.size()-currentChickenNum)<m) return;
+	public static void dfs(int currentChickenNum) {
 		
-		if(currentChickenNum==chicken.size()) {
-			if(selectedChicken.size()==m) {
-				answer=Math.min(answer, calculateDistance(selectedChicken));
-			}
+		
+		if(selectedChickenList.size()==maxChickenNum) {
+			answer=Math.min(answer, calculateDistance());
 			return;
 		}
 		
+		if (currentChickenNum == chickenList.size()) return;
 		
-		selectedChicken.add(chicken.get(currentChickenNum));
-		dfs(currentChickenNum+1,selectedChicken);
-		selectedChicken.remove(selectedChicken.size()-1);
+		// 유지하는 경우
+		selectedChickenList.add(chickenList.get(currentChickenNum));
+		dfs(currentChickenNum+1);
+		selectedChickenList.remove(selectedChickenList.size()-1);
 		
-		dfs(currentChickenNum+1,selectedChicken);
+		// 폐업하는 경우
+		dfs(currentChickenNum+1);
 	}
 	
-	public static int calculateDistance(List<Point> selectedChicken) {
-		int sum=0;
+	public static int calculateDistance() {
+		// 한 집에 대해서 최소 치킨집 거리를 구하는 것이므로
+		// outer for문에 homeList, inner for문에 selectedChickenList
+		// 들가는 게 맞음
 		
-		for(Point h: home) {
-			int min=Integer.MAX_VALUE;
-			for(Point c:selectedChicken) {
-				int distance=Math.abs(h.x-c.x)+Math.abs(h.y-c.y);
-				min=Math.min(min, distance);
+		int totalDistance=0;
+		
+		for(Point home:homeList) {
+			int minDistance=Integer.MAX_VALUE;
+			for(Point selectedChicken:selectedChickenList) {
+				int distance=Math.abs(selectedChicken.x-home.x)+Math.abs(selectedChicken.y-home.y);
+				minDistance=Math.min(minDistance, distance);
 			}
-			sum+=min;
+			
+			totalDistance+=minDistance;
 		}
 		
-		return sum;
+		return totalDistance;
 	}
 }
