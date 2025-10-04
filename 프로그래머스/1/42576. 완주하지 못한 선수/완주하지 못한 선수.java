0b6af793ec270b9abef0cac4dfb4f1f1a19a0b1c
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-10-04 14:19:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.30 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer="";
+        HashMap<String,Integer> hashMap=new HashMap<>();
+        
+        // participant 돌면서 
+        for(String p:participant){
+            hashMap.put(p,hashMap.getOrDefault(p,0)+1);
+        }
+        
+        for(String c:completion){
+            hashMap.put(c,hashMap.get(c)-1);
+        }
+        
+        for(String key:hashMap.keySet()){
+            if(hashMap.get(key)!=0){
+                answer=key;
+            }
+        }
+        
+        return answer;
+    }
+}
