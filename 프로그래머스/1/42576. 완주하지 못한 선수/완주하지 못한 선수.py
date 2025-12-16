5f5fe6f4fd1fe15db288edf5ac4236e2e05bd6f4
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-12-16 23:02:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.08 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,14 @@
-from collections import Counter
-
 def solution(participant, completion):
-    answer=Counter(participant)-Counter(completion)
-    return list(answer.keys())[0]
+    d={}
+    for p in participant:
+        d[p]=d.get(p,0)+1
+    
+    for c in completion:
+        d[c]-=1
+    
+    for k, v in d.items():
+        if v!=0:
+            return k
     
     
     
