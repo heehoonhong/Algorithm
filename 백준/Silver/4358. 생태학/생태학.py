492ae10c11e098b9595bf4246c5cd7f8ff16251b
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-12-26 15:10:00 +0900

    [Silver II] Title: 생태학, Time: 336 ms, Memory: 99708 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.py
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -1,13 +1,10 @@
 import sys
+from collections import Counter
 
 lines=sys.stdin.readlines()
-species={}
-cnt=0
-for line in lines:
-    tree=line.strip()
-    species[tree]=species.get(tree,0)+1
-    cnt+=1
 
+species=Counter(line.strip() for line in lines)
+cnt=sum(species.values())
 
 sorted_keys=sorted(species.keys())
 
