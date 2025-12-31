99cf4771c9c0571c0a0ce00b199cb19db1f9505d
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2025-12-31 17:03:44 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,30 @@
 import sys
 from collections import deque
 
-queue=deque()
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+def bfs(sx,sy):
+    queue=deque()
+    queue.append((sx,sy))
 
-n,m=map(int,sys.stdin.readline().split())
+    visited[sx][sy]=1
+    while queue:
+        x,y=queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
 
-graph=[list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
-visited=[[0]*m for _ in range(n)]
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny]==1 and visited[nx][ny]==0:
+                    visited[nx][ny]=visited[x][y]+1
+                    queue.append((nx,ny))
+    return visited[n-1][m-1]
 
-queue.append((0,0))
-visited[0][0]=1
 
-while queue:
-    x,y=queue.popleft()
-    #visited[x][y]=1
-    for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if graph[nx][ny]==1 and visited[nx][ny]==0:
-                visited[nx][ny]=visited[x][y]+1
-                queue.append((nx,ny))
-print(visited[n-1][m-1])
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+print(bfs(0,0))
