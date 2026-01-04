253349eb9dca8d65082aeeb35ab21156b51e84d0
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-01-04 17:19:26 +0900

    [level 1] Title: 잡은 물고기의 평균 길이 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
@@ -0,0 +1,9 @@
+with len as (
+    select 
+    case when length is null then 10
+    else length 
+    end as length
+    from fish_info
+)
+
+select round(avg(length),2) as average_length from len
