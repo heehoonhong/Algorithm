646149765bd36c6bddfabbc2a2564dcfd4923687
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-01-05 16:35:08 +0900

    [Gold IV] Title: 강의실 배정, Time: 344 ms, Memory: 75524 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,17 +1,19 @@
 import sys
 import heapq
 
-n=int(sys.stdin.readline())
-class_list=[]
 min_hq=[]
+class_list=[]
+n=int(sys.stdin.readline())
 for _ in range(n):
     start,end=map(int,sys.stdin.readline().split())
     class_list.append((start,end))
-class_list.sort(key=lambda x: (x[0],x[1]))
+
+class_list.sort(key=lambda x:(x[0],x[1]))
+
 heapq.heappush(min_hq,class_list[0][1])
 
 for i in range(1,n):
-    if min_hq[0]>class_list[i][0]:
+    if min_hq[0] > class_list[i][0]:
         heapq.heappush(min_hq,class_list[i][1])
     else:
         heapq.heappop(min_hq)
