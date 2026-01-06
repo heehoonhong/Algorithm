12d9d530d24122863c052b081a937a606fee8735
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-01-06 10:59:16 +0900

    [Gold V] Title: 행복 유치원, Time: 332 ms, Memory: 66624 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -1,22 +1,17 @@
 import sys
 
 n,k=map(int,sys.stdin.readline().split())
-
 children=list(map(int,sys.stdin.readline().split()))
-
 children=list(set(children))
-
 children.sort()
 
-adj_height=[]
-
+dis=[]
 for i in range(len(children)-1):
-    adj_height.append(children[i+1]-children[i])
+    dis.append(children[i+1]-children[i])
+dis.sort()
 
-adj_height.sort()
+for i in range(k-1):
+    if dis:
+        dis.pop(-1)
 
-for _ in range(k-1):
-    if adj_height:
-        adj_height.pop(-1)
-
-print(sum(adj_height))
+print(sum(dis))
