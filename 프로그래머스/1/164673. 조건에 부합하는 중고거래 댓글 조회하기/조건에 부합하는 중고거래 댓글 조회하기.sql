5057e2344cf155707e0223aa56c6a26d813d10d9
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-01-06 20:13:51 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,7 @@
-SELECT TITLE,B.BOARD_ID,R.REPLY_ID,R.WRITER_ID,R.CONTENTS,DATE_FORMAT(R.CREATED_DATE,'%Y-%m-%d') AS CREATED_DATE FROM USED_GOODS_BOARD B
-JOIN USED_GOODS_REPLY R
-ON B.BOARD_ID= R.BOARD_ID 
-WHERE B.CREATED_DATE LIKE '2022-10%'
-ORDER BY CREATED_DATE,B.TITLE
+select b.title,b.board_id,r.reply_id,r.writer_id,r.contents,
+date_format(r.created_date,'%Y-%m-%d') 
+from used_goods_board b
+join used_goods_reply r
+on b.board_id=r.board_id
+where year(b.created_date)=2022 and month(b.created_date)=10
+order by r.created_date, b.title
