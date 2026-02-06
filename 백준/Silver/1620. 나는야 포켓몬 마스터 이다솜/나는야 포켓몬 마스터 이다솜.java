d4287993e48ce78a5726cee9fe62301a80e7f351
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-06 17:07:09 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 508 ms, Memory: 53948 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,42 +1,45 @@
 //package ctp;
 
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 
 public class Main {
+   
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		Map<String,Integer> pokes=new HashMap<>();
+		Map<Integer,String> pokesReverse=new HashMap<>();
+		
+		for(int i = 0;i<n;i++) {
+			String poke=br.readLine();
+			pokes.put(poke, pokes.getOrDefault(poke, 0)+(i+1));
+			pokesReverse.put(i+1,poke );
+		}
+		
+		for(int i = 0;i<m;i++) {
+			String line=br.readLine();
+			if(Character.isAlphabetic(line.charAt(0))){
+				bw.write(pokes.get(line)+"\n");
+			}
+			else {
+				int dd=Integer.parseInt(line);
+				bw.write(pokesReverse.get(dd)+"\n");
+			}
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
 	
 	
-	
-    public static void main(String[] args) throws Exception{
-    	Scanner sc=new Scanner(System.in);
-    	int n=sc.nextInt();
-    	int m=sc.nextInt();
-    	sc.nextLine();
-    	Map<String,Integer> dogam=new HashMap<>();
-    	Map<Integer,String> reversedDogam=new HashMap<>();
-    	List<Object> answers=new ArrayList<>();
-    	
-    	for(int i = 0;i<n;i++) {
-    		String line=sc.nextLine();
-    		dogam.put(line, i+1);
-    		reversedDogam.put(i+1, line);
-    	}
-    	
-    	for(int i = 0;i<m;i++) {
-    		String line=sc.nextLine();
-    		if(Character.isAlphabetic(line.charAt(0))) {
-    			answers.add(dogam.get(line));
-    		}
-    		else {
-    			answers.add(reversedDogam.get(Integer.parseInt(line)));
-    		}
-    	}
-    	
-    	for(int i = 0;i<answers.size();i++) {
-    		System.out.println(answers.get(i));
-    	}
-    	
-    }	
 }
+
