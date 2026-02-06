3d1028b94d84b232b8f5b605627b8d3f75b511e7
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-06 16:38:50 +0900

    [level 3] Title: 베스트앨범, Time: 3.83 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
 
 ### 성능 요약
 
-메모리: 9.11 MB, 시간: 0.11 ms
+메모리: 85 MB, 시간: 3.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 06일 15:32:30
+2026년 02월 06일 16:38:49
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,51 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    public static class Album implements Comparable<Album>{
+    public static class Album{
         
-        int index, playCount;
-        public Album(int index,int playCount){
+        int play;
+        int index;
+        public Album(int play,int index){
+            this.play=play;
             this.index=index;
-            this.playCount=playCount;
         }
         
-        @Override
-        public int compareTo(Album otherAlbum){
-            if(otherAlbum.playCount==this.playCount){
-                return this.index-otherAlbum.index;
-            }
-            else{
-                return otherAlbum.playCount-this.playCount;
-            }
-        }
     }
     
     public int[] solution(String[] genres, int[] plays) {
-        int len=genres.length;
-        Map<String,Integer> genreRank=new HashMap<>();
-        Map<String,List<Album>> albumRank=new HashMap<>();
         
-        // 데이터 초기화
-        for(int i=0;i<len;i++){
-            genreRank.put(genres[i],genreRank.getOrDefault(genres[i],0)+plays[i]);
-            albumRank.putIfAbsent(genres[i],new ArrayList<>());
-            albumRank.get(genres[i]).add(new Album(i,plays[i]));
+        Map<String,Integer> genresMap=new HashMap<>();
+        Map<String,List<Album>> albums=new HashMap<>();
+        
+        for(int i = 0;i<genres.length;i++){
+            genresMap.put(genres[i],genresMap.getOrDefault(genres[i],0)+plays[i]);
+            // 일단 이 해시맵의 value가 리스트니까 key를 먼저 넣고 
+            // 그 다음에 그 key에 대한 값을 넣어야 함.
+            albums.putIfAbsent(genres[i],new ArrayList<>());
+            albums.get(genres[i]).add(new Album(plays[i],i));
         }
+        List<Integer> result=new ArrayList<>();
+        // 일단 장르에 대한 정렬이 필요하므로 장르 정렬해야 함.
+        List<String> genresList=new ArrayList<>(genresMap.keySet());
+        genresList.sort((g1,g2)->genresMap.get(g2)-genresMap.get(g1) );
         
-        // 장르의 플레이 수를 기준으로 정렬
-        List<String> sortedGenres=new ArrayList<>(genreRank.keySet());
-        sortedGenres.sort((a,b)->genreRank.get(b)-genreRank.get(a));
-        
-        List<Integer> ans = new ArrayList<>();
-        
-        for(String genre:sortedGenres){
-            List<Album> albumList=albumRank.get(genre);
-            Collections.sort(albumList);
-            for(int i=0;i<Math.min(2,albumList.size());i++){
-                ans.add(albumList.get(i).index);
+        for(String genre:genresList){
+            List<Album> albumList=albums.get(genre);
+            albumList.sort( (a,b)-> {
+                if(a.play==b.play){
+                    return a.index-b.index;                    
+                }
+                else{
+                    return b.play-a.play;
+                }
+            });
+            // 정렬 후 result에 index 저장
+            for(int i =0;i<Math.min(2,albumList.size());i++){
+                result.add(albumList.get(i).index);
             }
         }
+        int[] answer=new int[result.size()];
+        for(int i = 0;i<result.size();i++){
+            answer[i]=result.get(i);
+        }
         
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
