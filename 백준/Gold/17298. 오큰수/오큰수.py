1b66d5b07de4dc90a7110b50549853c6c0e7e66d
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-09 21:23:53 +0900

    [Gold IV] Title: 오큰수, Time: 1168 ms, Memory: 187420 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,17 +1,27 @@
 import sys
 
-n=int(sys.stdin.readline())
-nums=list(map(int,sys.stdin.readline().split()))
-result=[-1]*n
+input=sys.stdin.readline
+n=int(input())
+a=list(map(int,input().split()))
 stack=[]
-stack.append((nums[0],0))
-
-for i in range(1,n):
-    while stack and nums[i]>stack[-1][0]:
-        result[stack[-1][1]]=nums[i]
+result=[-1]*n
+for i in range(len(a)-1):
+    # a[i]가 오큰수의 자격이 있는지
+    while stack and stack[-1][0]<a[i]:
+        result[stack[-1][1]]=a[i]
         stack.pop()
+    stack.append((a[i],i))
 
-    stack.append((nums[i],i))
+#print(*stack)
+#print(*result)
 
-result[-1]=-1
+while stack:
+    if stack[-1][0]<a[-1]:
+        result[stack[-1][1]]=a[-1]
+    else:
+        result[stack[-1][1]]=-1
+    stack.pop()
+
+#print("==========")
+#print(*stack)
 print(*result)
