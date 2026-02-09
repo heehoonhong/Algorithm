16e9baf3291ef8e6a8376e3c7f6a6747225ca698
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-09 18:05:50 +0900

    [level 2] Title: 주식가격, Time: 45.50 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
 
 ### 성능 요약
 
-메모리: 18.9 MB, 시간: 46.74 ms
+메모리: 76.4 MB, 시간: 45.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 09일 16:08:04
+2026년 02월 09일 18:05:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,43 @@
 import java.util.*;
 
 class Solution {
+    public static class Price{
+        int price;
+        int index;
+        public Price(int price,int index){
+            this.price=price;
+            this.index=index;
+        }
+    }
+    
     public int[] solution(int[] prices) {
-        // 변수명 오타 수정: stockPeried -> stockPeriod
-        int[] stockPeriod = new int[prices.length];
-        
-        for (int i = 0; i < prices.length; i++) {
-            // j는 마지막 인덱스까지 확인해야 함
-            for (int j = i + 1; j < prices.length; j++) {
-                // 1. 일단 1초가 흘렀으므로 기간을 1 증가시킴
-                stockPeriod[i]++;
-                
-                // 2. 만약 현재 가격이 다음 가격보다 비싸면(가격 하락)
-                if (prices[i] > prices[j]) {
-                    // 3. 더 이상 확인할 필요가 없으므로 반복을 중단(break)
-                    break;
+        Stack<Price> stack=new Stack<>();
+        int[] result=new int[prices.length];
+        stack.add(new Price(prices[0],0));
+        for(int i = 1;i<prices.length;i++){
+            if(stack.peek().price<=prices[i]){
+                stack.add(new Price(prices[i],i));
+            }
+            else{
+                while(!stack.isEmpty() && stack.peek().price>prices[i]){
+                    result[stack.peek().index]=i-stack.peek().index;
+                    stack.pop();
                 }
+                stack.add(new Price(prices[i],i));
             }
         }
-        return stockPeriod;
+        if(!stack.isEmpty()){
+            Price[] ss=new Price[stack.size()];
+            for(int i=stack.size()-1;i>=0;i--){
+                Price price2=stack.pop();
+                ss[i]=price2;
+            }
+            for(int i = 0;i<ss.length;i++){
+                result[ss[i].index]=(ss[ss.length-1].index)-(ss[i].index);
+            }
+        
+        }
+        return result;
+        
     }
 }
