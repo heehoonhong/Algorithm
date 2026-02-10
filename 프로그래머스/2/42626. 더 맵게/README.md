2e1164b903168a4544fd4f7d9fa5cf9654120103
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-10 15:45:01 +0900

    [level 2] Title: 더 맵게, Time: 1614.78 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
 
 ### 성능 요약
 
-메모리: 50.7 MB, 시간: 1873.52 ms
+메모리: 128 MB, 시간: 1614.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 09일 16:31:34
+2026년 02월 10일 15:44:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,32 +1,26 @@
 import java.util.*;
 
 class Solution {
-    
-    public static Queue<Integer> queue;
-    
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        
-        queue=new PriorityQueue<>();
-        for(int i=0;i<scoville.length;i++){
-            // 데이터 삽입
-            queue.offer(scoville[i]);
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i = 0;i<scoville.length;i++){
+            pq.offer(scoville[i]);
         }
-        
-        while(queue.peek()<K){
-            
-            if(queue.size()==1){
-                answer=-1;
-                break;
+        int sco=0;
+        int cnt=0;
+        boolean flag=true;
+        while(!pq.isEmpty() && pq.peek()<K){
+            if(pq.size()<2){
+                return -1;
             }
+            int first=pq.poll();
+            int second=pq.poll();
+            pq.offer(first+2*second);
             
-            int first=queue.poll();
-            int second=queue.poll();
-            int newFood=first+2*second;
-            queue.offer(newFood);
-            answer++;
+            cnt++;
         }
         
-        return answer;
+        return cnt;
     }
 }
