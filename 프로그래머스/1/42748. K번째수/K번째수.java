c517550500cfa93e6af3b5d0644827aed02eed7d
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-11 13:31:28 +0900

    [level 1] Title: K번째수, Time: 1.17 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int index=0;
+        for(int i = 0;i<commands.length;i++){
+            int[] newArr=Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(newArr);
+            answer[i]=newArr[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
