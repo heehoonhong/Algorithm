f3e047aba7a20dae49e50e6cb09d9cb4f8213f9f
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-02-19 13:05:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.05 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 9.38 MB, 시간: 10.99 ms
+메모리: 54.8 MB, 시간: 8.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 19일 12:52:18
+2026년 02월 19일 13:05:08
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,62 +1,49 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
+    public static int n,m;
+    public static int[][] visited;
+    public static int[] dx={-1,1,0,0};
+    public static int[] dy={0,0,1,-1};
     
     public static class Point{
-        int x,y;
-        
-        Point(int x,int y){
+        int x;
+        int y;
+        public Point(int x,int y){
             this.x=x;
             this.y=y;
         }
     }
-    
-    public static int[] dx={-1,1,0,0};
-    public static int[] dy={0,0,-1,1};
-    public static int[][] visited;
-    public static int answer;
-    public static int n,m;
-    public static Queue<Point> queue=new LinkedList<>();
-    public int solution(int[][] maps) {
-        answer = 0;
+    public static void bfs(int x,int y,int [][] maps){
+        Queue<Point> q=new LinkedList<>();
+        q.offer(new Point(x,y));
+        visited[x][y]=1;
         
-        // 방문 배열 초기화
-        visited=new int[maps.length][maps[0].length];
-        
-        bfs(maps);
-        
-        if(visited[n-1][m-1]!=0){
-            return visited[n-1][m-1];
-        }
-        else{
-            return -1;
-        }
-        
-        //return answer;
-    }
-    
-    public static void bfs(int[][] maps){
-        n=maps.length;
-        m=maps[0].length;
-        
-        queue.offer(new Point(0,0));
-        visited[0][0]=1;
-        
-        while(!queue.isEmpty()){
-            Point p=queue.poll();
-            for(int i=0;i<4;i++){
-                int nx=p.x+dx[i];
-                int ny=p.y+dy[i];
-                if(nx>=0&&nx<n&&ny>=0&&ny<m){ // 주어진 범위 안에 있고
-                    if(visited[nx][ny]==0&&maps[nx][ny]==1){
-                        queue.offer(new Point(nx,ny));
-                        visited[nx][ny]+=visited[p.x][p.y]+1;
-                        //answer++;
-                    }
+        while(!q.isEmpty()){
+            Point p=q.poll();
+            for(int i = 0;i<4;i++){
+                int nx=dx[i]+p.x;
+                int ny=dy[i]+p.y;
+                if(nx>=0 && nx<n && ny>=0 && ny<m && visited[nx][ny]==0 && maps[nx][ny]==1){
+                    q.offer(new Point(nx,ny));
+                    visited[nx][ny]=visited[p.x][p.y]+1;
                 }
             }
         }
+    }
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
         
+        n=maps.length;
+        m=maps[0].length;
+        visited=new int[n][m];
+        bfs(0,0,maps);
+        if(visited[n-1][m-1]==0){
+            return -1;
+        }
+        else{
+            return visited[n-1][m-1];
+        }
     }
 }
