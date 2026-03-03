130c6e210c76ce63c9325aa0cc214c6bf19056ba
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-03 14:03:02 +0900

    [Gold III] Title: 치즈, Time: 432 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -2,12 +2,14 @@ import sys
 from collections import deque
 
 input=sys.stdin.readline
+n,m=map(int,input().split())
+cheese_cnt=0
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-n,m=map(int,input().split())
+# 0이면 방문 처리하고, nx,ny 가 1이라면 이전 것들이 다 0일테니까 visited를 통해
+# 방문 처리를 하고, 1 중에서 visited가 2이상이면 외부 공기와 2면 이상 접촉한 거임
+
 graph=[[0]*m for _ in range(n)]
-cheese_cnt=0
-cnt=0
 for i in range(n):
     line=list(map(int,input().split()))
     for j in range(m):
@@ -17,11 +19,11 @@ for i in range(n):
 
 def bfs():
     global cheese_cnt
-    visited=[[0]*m for _ in range(n)]
     q=deque()
     q.append((0,0))
-    visited[0][0]=1
+    visited=[[0]*m for _ in range(n)]
     cheese_q=deque()
+    visited[0][0]=1
 
     while q:
         x,y=q.popleft()
@@ -32,29 +34,27 @@ def bfs():
                 if visited[nx][ny]==0 and graph[nx][ny]==0:
                     q.append((nx,ny))
                     visited[nx][ny]=1
-                elif graph[nx][ny]==1:
+                if graph[nx][ny]==1:
+                    # 이렇게 하면 방문 처리를 안 해서 중복으로 nx,ny 좌표가 담길 수 있음
+                    # 이렇게 하는 것보단 다시 전부를 돌면서 2 이상인 걸 처리하면 될 듯? 
+                    cheese_q.append((nx,ny))
                     visited[nx][ny]+=1
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j]==1 and visited[i][j]>=2:
-                graph[i][j]=0
-                cheese_cnt-=1
+    cheese_q=deque(set(cheese_q))
+    while cheese_q:
+        x,y=cheese_q.popleft()
+        if visited[x][y]>=2:
+            graph[x][y]=0
+            cheese_cnt-=1
+    #print(cheese_cnt)
+    #print(*graph)
 
-
-    #for i in range(n):
-    #    for j in range(m):
-    #        print(graph[i][j],end=' ')
-    #    print()
-    #print("===========")
-
-
-
-
-
+cnt=0
 while True:
+
     bfs()
     cnt+=1
-    #print(cheese_cnt)
-    if cheese_cnt<=0: break
+    #print(cnt)
+    if cheese_cnt<=0:
+        break
 
 print(cnt)
