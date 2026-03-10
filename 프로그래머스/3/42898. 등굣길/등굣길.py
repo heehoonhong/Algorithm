947d84a22770aa84402952fa4530e012ca311a75
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-10 11:19:00 +0900

    [level 3] Title: 등굣길, Time: 2.75 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -2,6 +2,8 @@ def solution(m, n, puddles):
     # dp[i][j] (i,j) 까지 갈 수 있는 경우의 수
     dp=[[0]*(m+1) for _ in range(n+1)]
     dp[1][1]=1
+    for i in range(len(puddles)):
+        dp[puddles[i][1]][puddles[i][0]]=-1
     
     for i in range(1,n+1):
         for j in range(1,m+1):
@@ -9,15 +11,13 @@ def solution(m, n, puddles):
                 dp[i][j]=1
                 dp[i][j]=dp[i][j]%1000000007
                 continue
-            else:
+            
+            if dp[i][j]!=-1:
                 dp[i][j]=dp[i-1][j]+dp[i][j-1]
                 dp[i][j]=dp[i][j]%1000000007
-                
+            else:
+                dp[i][j]=0
             
-            for puddle in puddles:
-                x,y=puddle
-                if x==j and y==i:
-                    dp[i][j]=0
             #print(dp[i][j],end=' ')
         #print()
     return dp[n][m]
