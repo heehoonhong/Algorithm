4bb08a4b4ab73552bb45ea6ac8e0c1a6e404aa1a
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-10 14:20:25 +0900

    [level 3] Title: 거스름돈, Time: 640.41 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,9 @@
+def solution(n, money):
+    money.sort()
+    dp=[0]*(n+1)
+    dp[0]=1
+    for m in money:
+        for j in range(m,n+1):
+            dp[j]=dp[j]+dp[j-m]
+            dp[j]=dp[j]%1000000007
+    return dp[n]
