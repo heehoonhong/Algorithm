7f2b0b5422350d9047cbeebe828c0310600265a3
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-12 10:18:19 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 1158.29 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -0,0 +1,22 @@
+def solution(board):
+    col,row=len(board),len(board[0])
+    dp=[[0]*row for _ in range(col) ]
+    result=0
+    # dp[i][j]: i부터 j까지의 최대 행 개수
+    for i in range(col):
+        for j in range(row):
+            if i==0 or j==0:
+                if board[i][j]==1:
+                    dp[i][j]=1
+            else:
+                garo=min(dp[i-1][j],dp[i-1][j-1])
+                sero=min(dp[i-1][j-1],dp[i][j-1])
+                res=min(garo,sero)
+                if board[i][j]==1:
+                    dp[i][j]=res+1
+            result=max(dp[i][j],result)
+            #print(dp[i][j],end=' ')
+        #print()
+    return result*result    
+            
+    
