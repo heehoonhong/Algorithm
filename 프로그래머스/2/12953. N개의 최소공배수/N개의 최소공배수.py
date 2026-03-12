2153427cabe4e637e209241fb9ce6bf499324fa5
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-12 14:10:20 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.17 ms, Memory: 9.75 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,15 +1,8 @@
+from fractions import gcd
+
 def solution(arr):
-    def gcd(n,m):
-        if m==0:
-            return n
-        else:
-            return gcd(m,n%m)
-    
-    def lcm(n,m):
-        return n//gcd(n,m)*m    
-    
-    l=lcm(arr[0],arr[1])
-    for i in range(len(arr)-1):
-        l=lcm(l,arr[i+1])
-    return l
+    num=arr[0]
+    for n in arr:
+        num=num*n/gcd(n,num)
+    return num
         
