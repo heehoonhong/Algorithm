0a276e1086b36277a095973a1acc53eba58e118e
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-17 10:57:27 +0900

    [Silver II] Title: 차이를 최대로, Time: 128 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -4,11 +4,10 @@ from itertools import permutations
 input=sys.stdin.readline
 n=int(input())
 a=list(map(int,input().split()))
-max_num=0
-for e in permutations(a):
-    elst=list(e)
-    cnt=0
+ans=0
+for l in permutations(a):
+    total=0
     for i in range(n-1):
-        cnt+=abs(elst[i]-elst[i+1])
-    max_num=max(max_num,cnt)
-print(max_num)
+        total+=abs(l[i]-l[i+1])
+    ans=max(total,ans)
+print(ans)
