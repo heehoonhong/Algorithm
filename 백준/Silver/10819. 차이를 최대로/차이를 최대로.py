7d71a9ea966abbd649601b9608ff7b3cf6380160
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-17 11:06:48 +0900

    [Silver II] Title: 차이를 최대로, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,13 +1,29 @@
 import sys
-from itertools import permutations
 
 input=sys.stdin.readline
 n=int(input())
 a=list(map(int,input().split()))
+visited=[0]*n
 ans=0
-for l in permutations(a):
-    total=0
-    for i in range(n-1):
-        total+=abs(l[i]-l[i+1])
-    ans=max(total,ans)
+def dfs(depth,arr):
+    global ans
+    if depth==n:
+        total=0
+        for i in range(n-1):
+            total+=abs(arr[i]-arr[i+1])
+        ans=max(ans,total)
+        return
+
+    for i in range(n):
+        if visited[i]==0:
+            visited[i]=1
+            dfs(depth+1,arr+[a[i]])
+            visited[i]=0
+
+
+for i in range(n):
+    if visited[i]==0:
+        visited[i]=1
+        dfs(1,[]+[a[i]])
+        visited[i]=0
 print(ans)
