43e4ff9d46818772431d07e927cb1a85500ac389
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-23 14:02:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,14 +1,10 @@
-from collections import defaultdict
-
 def solution(n, words):
     s=[]
-    p=defaultdict(list)
     s.append(words[0])
     for i in range(1,len(words)):
         if words[i] not in s and words[i][0]==words[i-1][-1]:
             s.append(words[i])
         else:
-            #print(i)
             return [(i%n)+1,(i//n)+1]
     
     return [0,0]
