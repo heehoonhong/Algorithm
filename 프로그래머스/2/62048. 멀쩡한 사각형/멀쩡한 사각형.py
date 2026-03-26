cbaa3837ab0d1824aaecfa3187dbc6ffa675616d
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-26 11:07:42 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.41 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,6 +1,7 @@
 from fractions import gcd
 
 def solution(w,h):
-    ww=w//gcd(w,h)
-    hh=h//gcd(w,h)
-    return w*h-gcd(w,h)*(ww+hh-1)
+    #ww=w//gcd(w,h)
+    #hh=h//gcd(w,h)
+    #return w*h-gcd(w,h)*(ww+hh-1)
+    return w*h-w-h+gcd(w,h)
