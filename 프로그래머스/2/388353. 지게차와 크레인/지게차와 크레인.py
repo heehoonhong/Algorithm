40e0ae11701b833af6796dd6d8397f44acd2373b
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-26 16:48:25 +0900

    [level 2] Title: 지게차와 크레인, Time: 7368.15 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -0,0 +1,77 @@
+from collections import deque
+
+def solution(storage, requests):
+    # requests[i]==1일 때는 접근 가능한 알파벳들만 없애기
+    # requests[i]==2일 때는 그 알파벳에 해당하는 모든 위치들에 대해서 없애기
+    n=len(storage)
+    m=len(storage[0])
+    dx=[0,0,1,-1]
+    dy=[1,-1,0,0]
+    graph=[['0']*m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            graph[i][j]=storage[i][j]
+    
+    
+    def can_reach(tx,ty):
+        if tx==0 or tx==n-1 or ty==0 or ty==m-1: return True
+        visited=[[0]*m for _ in range(n)]
+        q=deque()
+        for i in range(n):
+            if graph[i][0]=='0':
+                q.append((i,0))
+                visited[i][0]=1
+            if graph[i][m-1]=='0':
+                q.append((i,m-1))
+                visited[i][m-1]=1
+        
+        for j in range(m):
+            if graph[0][j]=='0':
+                q.append((0,j))
+                visited[0][j]=1
+            if graph[n-1][j]=='0':
+                q.append((n-1,j))
+                visited[n-1][j]=1
+            
+    
+        # q에서 하나씩 pop하면서
+        # 상하좌우에 tx,ty인지 탐색하고
+        # q에 append하는 건 빈 공간만 append
+        while q:
+            x,y=q.popleft()
+            # tx, ty 찾기
+            for i in range(4):
+                nx,ny=x+dx[i],y+dy[i]
+                if 0<=nx<n and 0<=ny<m and (nx==tx and ny==ty) and graph[nx][ny]!='0':
+                    return True
+            
+            for i in range(4):
+                nx,ny=x+dx[i],y+dy[i]
+                if 0<=nx<n and 0<=ny<m and visited[nx][ny]==0 and graph[nx][ny]=='0':
+                    visited[nx][ny]=1
+                    q.append((nx,ny))
+        return False
+            
+            
+    
+    for index in range(len(requests)):
+        s=[]
+        if len(requests[index])==1:
+            for i in range(n):
+                for j in range(m):
+                    if graph[i][j]==requests[index][0] and can_reach(i,j):
+                        s.append((i,j))
+                    
+        else:
+            for i in range(n):
+                for j in range(m):
+                    if graph[i][j]==requests[index][0]:
+                        #graph[i][j]='0'
+                        s.append((i,j))
+        for (xx,yy) in s:
+            graph[xx][yy]='0'
+    result=0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]!='0': result+=1
+    return result
