34d35640a4e92b7982ff5b497b4f8d5261bd4b3d
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-30 21:55:16 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,5 +1,7 @@
-select id,email,first_name,last_name from developers
-where 
-skill_code & (select code from skillcodes where name='Python') or
-skill_code & (select code from skillcodes where name='C#')
-order by id
+select id,email,first_name,last_name
+from developers
+where skill_code&(select code from skillcodes where name='Python')=
+(select code from skillcodes where name='Python') or 
+skill_code&(select code from skillcodes where name='C#')=
+(select code from skillcodes where name='C#')
+order by 1
