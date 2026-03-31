08060c07b40a146829911c44b4a35f6b3b5ba1bc
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-03-31 10:57:14 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,22 +1,17 @@
 with recursive gen as (
-    select id, parent_id, 1 as depth
-    from ecoli_data
+    select id,parent_id,1 as depth from ecoli_data 
     where parent_id is null
-    
+
     union all
     
-    select c.id,c.parent_id, p.depth+1 depth
-    from gen as p
+    select c.id,c.parent_id, p.depth+1 as depth from gen as p
     join ecoli_data as c
-    on c.parent_id=p.id
-    
-    
+    on c.parent_id = p.id
 )
 
-select count(g.id) as COUNT, g.depth as GENERATION
-from gen as g
+select count(*) as count, p.depth as generation from gen as p
 left join ecoli_data as c
-on g.id=c.parent_id
+on c.parent_id=p.id
 where c.id is null
-group by g.depth
-order by g.depth
+group by depth
+order by depth
