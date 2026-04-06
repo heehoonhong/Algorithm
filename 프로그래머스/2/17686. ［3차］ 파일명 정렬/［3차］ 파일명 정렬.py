c7b683ec130e8f20ec31fec71cb7e1e5946d5927
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-04-06 16:52:37 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 7.94 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,38 +1,6 @@
+import re
+
 def solution(files):
-    arr=[]
-    
-    for file in files:
-        flag=False
-        start,end=0,0
-        for i in range(len(file)):
-            # 숫자 만나기 전
-            if not file[i].isdigit() and not flag:
-                continue
-            elif file[i].isdigit() and not flag:
-                start=i
-                flag=True
-            elif file[i].isdigit() and flag:
-                if i-start==5:
-                    end=i
-                    break
-                else:
-                    continue
-            elif not file[i].isdigit() and flag: 
-                end=i
-                break
-        if end==0: end=len(file) 
-                    
-        arr.append((start,end))
-    f=[]
-    for i in range(len(files)):
-        s,e=arr[i]
-        head,number,tail=files[i][:s],files[i][s:e],files[i][e:]
-        f.append((head,number,tail))
-    #print(f)
-    
-    answer=[]
-    f.sort(key=lambda x:(x[0].lower(),int(x[1])))
-    for i in range(len(f)):
-        result=''.join(f[i])
-        answer.append(result)
-    return answer
+    a = sorted(files, key=lambda file : int(re.findall('\d+', file)[0]))
+    b = sorted(a, key=lambda file : re.split('\d+', file.lower())[0])
+    return b
