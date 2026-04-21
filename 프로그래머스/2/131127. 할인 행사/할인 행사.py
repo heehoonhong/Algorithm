d01a555a7c3fd25fce1668f9b7aa4896b0d7594b
heehoonhong <107936088+heehoonhong@users.noreply.github.com>
2026-04-21 15:03:37 +0900

    [level 2] Title: 할인 행사, Time: 150.37 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,39 +1,14 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(want, number, discount):
-    # 마트: 매일 한 가지 상품 할인함
-    # 하루에 하나씩만 구매가능
-    # 원하는 제품과 수량이 할인하는 날짜와 10일 연속으로 일치
-    # -> 회원가입
-    products=defaultdict(int)
+    answer = 0
+    
+    dic={}
     for i in range(len(want)):
-        products[want[i]]=number[i]
+        dic[want[i]]=number[i]
+    for i in range(len(discount)-9):
+        if dic==Counter(discount[i:i+10]):
+            answer+=1
     
     
-    def check(pp,arr,index):
-        s=0
-        
-        for i in range(len(arr)):
-            p=arr[i]
-            if p not in pp.keys():
-                s=0
-                break
-            else:
-                if pp[p]>0: 
-                    s+=1
-                    pp[p]-=1
-                    
-        #print(s)
-        #print(index)
-        #print("======")
-        if s==10:
-            return True
-        else:
-            return False
-            
-    answer=0
-    for i in range(0,len(discount)-10+1):
-        pp=products.copy()
-        v=check(pp,discount[i:i+10],i)
-        if v: answer+=1
     return answer
